5bcdc7b9a6beb13d04b731ab882bdcb4e32775ee
Christoph Weber <WeberChristoph85@gmail.com>
2023-03-30 15:02:53 +0200

    testing

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,9 @@
-/*!
-* Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
 //
 // Scripts
 // 
-//import './bootstrap'
-window.addEventListener('DOMContentLoaded', event => {
+import './bootstrap'
+/*
+document.addEventListener('DOMContentLoaded', event => {
 
   // Navbar shrink function
   var navbarShrink = function () {
@@ -57,3 +53,5 @@ window.addEventListener('DOMContentLoaded', event => {
   });
 
 });
+
+*/
